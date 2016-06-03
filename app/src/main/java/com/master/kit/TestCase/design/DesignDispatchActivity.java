5ab1f57f45501f7cd8467cb85cabb168f15a0e13
lh994749769 <994749769@qq.com>
2016-06-03 18:03:35 +0800

    design..............................

---
+++ app/src/main/java/com/master/kit/TestCase/design/DesignDispatchActivity.java
@@ -0,0 +1,27 @@
+package com.master.kit.testcase.design;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.master.kit.base.BaseActivity;
+import com.master.kit.R;
+import com.master.kit.testcase.design.tab.TabActivity;
+
+public class DesignDispatchActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_design_dispatch);
+    }
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.DesignActivity:
+                startActivity(DesignActivity.class);
+                break;
+            case R.id.TabActivity:
+                startActivity(TabActivity.class);
+                break;
+        }
+    }
+}
---
+++ app/src/main/java/com/master/kit/TestCase/jaon/Test.java
@@ -0,0 +1,45 @@
+package com.master.kit.testcase.jaon;
+
+/**
+ * Created by master on 2016/4/19.
+ */
+public class Test {
+    /**
+     * {"CityId":18,"CityName":"西安","ProvinceId":27,"CityOrder":1}
+     */
+
+   /* {
+       Class city =  City.class;
+        Object o = null;
+
+        try {
+            Constructor c = city.getConstructor();
+            o = c.newInstance();
+
+            Field a  = city.getField("CityName");
+            a.set(o,"西安");
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        }
+        Field f  = null;
+        try {
+            f = city.getField("CityId");
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        }
+        try {
+            f.set(o,18);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }*/
+
+}
