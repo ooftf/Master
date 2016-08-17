fa5a9f7497c8ae6e2aa07916c9c7882dd43479fe
lh994749769 <994749769@qq.com>
2016-08-17 14:45:13 +0800

    更新相册工具

--- app/src/main/java/com/master/kit/TestCase/CameraActivity.java
+++ app/src/main/java/com/master/kit/TestCase/CameraActivity.java
@@ -1,6 +1,9 @@
 package com.master.kit.testcase;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Camera;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -8,21 +11,26 @@ import android.widget.ImageView;
 import com.master.kit.base.BaseActivity;
 import com.master.kit.R;
 import com.master.kit.utils.AlbumUtil;
+import com.master.kit.utils.BitmapUtils;
+import com.master.kit.utils.CameraUtil;
 import com.master.kit.utils.LogUtil;
+
+import java.io.File;
 
 public class CameraActivity extends BaseActivity {
 
     private ImageView imageView;
-
+    CameraUtil cameraUtil;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+        cameraUtil = new CameraUtil(CameraActivity.this,100);
         findViewById(R.id.btn_main).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               // CameraUtil.takePhoto(CameraActivity.this, Environment.getExternalStorageDirectory().getPath(),"aa",1000);
-                AlbumUtil.selectPhoto(CameraActivity.this,100);
+               // CameraUtil.takePhoto(CameraActivity.this, Environment.getExternalStorageDirectory().getPath(),"aa",1000)
+                cameraUtil.openCamera();
             }
         });
         imageView = (ImageView) findViewById(R.id.iv_main);
@@ -31,9 +39,22 @@ public class CameraActivity extends BaseActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        LogUtil.e("Intent",data+"");
-        if(resultCode == 0) return;
-        imageView.setImageBitmap(AlbumUtil.getBitmapFromIntent(this,data));
+        cameraUtil.handleActivityResult(requestCode, resultCode,new CameraUtil.Callbacks() {
+            @Override
+            public void onImagePicked(File imageFile) {
+                imageView.setImageBitmap(BitmapUtils.readBitmapFromPath(imageFile.getPath(),800*800));
+            }
+
+            @Override
+            public void onImagePickerError(Exception e) {
+                LogUtil.e("我错了");
+            }
+
+            @Override
+            public void onCanceled() {
+                LogUtil.e("你居然干了这种事情");
+            }
+        });
     }
 
 }
