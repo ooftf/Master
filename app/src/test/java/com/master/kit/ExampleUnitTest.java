95b30b1141358186e86e45a13c80ff8a6abe1257
ooftf <994749769@qq.com>
2017-11-20 23:52:33 +0800

    版本库

--- app/src/test/java/com/master/kit/ExampleUnitTest.java
+++ app/src/test/java/com/master/kit/ExampleUnitTest.java
@@ -1,8 +1,10 @@
 package com.master.kit;
 
+import com.master.kit.testcase.banner.BannerBean;
+
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
@@ -10,6 +12,27 @@ import static org.junit.Assert.*;
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
+        System.out.println("sbs6-----" + BannerBean.class.getSimpleName().hashCode());
+        System.out.println("sbs7-----" + BannerBean.class.getName().hashCode());
+        System.out.println("sbs8-----" + BannerBean.class.getSimpleName().hashCode());
+        System.out.println("sbs8-----" + "BannerBean".hashCode());
+        assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void logCode() {
+        BannerBean bean = new BannerBean();
+        System.out.println("sb1-----" + bean.hashCode());
+        // Log.e("bean",""+bean.hashCode());
+        bean.setCode("sssssddd");
+        bean.setInfo("5fasdfsdafsfdsafd");
+        bean.setBody(new BannerBean.BodyEntity());
+        //Log.e("bean",""+bean.hashCode());
+        System.out.println("sb2-----" + bean.hashCode());
+        System.out.println("sb5-----" + new BannerBean().hashCode());
+        System.out.println("sbs6-----" + BannerBean.class.getSimpleName().hashCode());
+        System.out.println("sbs7-----" + BannerBean.class.getSimpleName().hashCode());
+        System.out.println("sbs8-----" + BannerBean.class.getSimpleName().hashCode());
         assertEquals(4, 2 + 2);
     }
 }
