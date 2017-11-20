6f385bc5067c551319a2fe6c0480540b2bd85133
ooftf <994749769@qq.com>
2017-11-21 01:30:34 +0800

    修复dagger问题

--- app/src/main/java/com/master/kit/fragment/OtherFragment.kt
+++ app/src/main/java/com/master/kit/fragment/OtherFragment.kt
@@ -1,14 +1,9 @@
 package com.master.kit.fragment
 
 import android.os.Bundle
-
-import com.master.kit.R
-import com.master.kit.testcase.AlbumActivity
-import com.master.kit.testcase.CameraActivity
+import com.master.kit.bean.ScreenItemBean
 import com.master.kit.testcase.annotation.AnnotationActivity
 import com.master.kit.testcase.softkeyboard.SoftKeyboardActivity
-
-import com.master.kit.bean.ScreenItemBean
 
 /**
  * Created by master on 2017/9/26 0026.
@@ -16,9 +11,7 @@ import com.master.kit.bean.ScreenItemBean
 
 class OtherFragment : BaseHomeFragment() {
     override fun initData() {
-        adapter.add(ScreenItemBean(AlbumActivity::class.java))
         adapter.add(ScreenItemBean(AnnotationActivity::class.java))
-        adapter.add(ScreenItemBean(CameraActivity::class.java))
         adapter.add(ScreenItemBean(SoftKeyboardActivity::class.java))
         adapter.notifyDataSetChanged()
     }
