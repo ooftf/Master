59bc22530c6cc72833303401b30a6a8101dc5317
99474 <994749769@qq.com>
2017-11-29 18:14:53 +0800

    将dialog位置设置为底部

--- app/src/main/java/com/master/kit/testcase/DialogDemo.java
+++ app/src/main/java/com/master/kit/testcase/DialogDemo.java
@@ -2,6 +2,7 @@ package com.master.kit.testcase;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.view.Gravity;
 import android.view.WindowManager;
 
 import com.master.kit.R;
@@ -13,10 +14,6 @@ public class DialogDemo extends Dialog {
         setContentView(R.layout.dialog_authorization);
         WindowManager.LayoutParams attributes = getWindow().getAttributes();
         attributes.width = (int) (getContext().getResources().getDisplayMetrics().widthPixels*0.8);
-        /*WindowManager.LayoutParams layoutParams = (WindowManager.LayoutParams) getWindow().getDecorView().getLayoutParams();;
-        layoutParams.width = (int) (getContext().getResources().getDisplayMetrics().widthPixels*0.8);
-        layoutParams.height = 200;
-        getWindow().getDecorView().setLayoutParams(layoutParams);
-        getWindow().getDecorView().setMinimumWidth((int) (getContext().getResources().getDisplayMetrics().widthPixels*0.8));*/
+        getWindow().setGravity(Gravity.BOTTOM);
     }
 }
