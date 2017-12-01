44dbb43719972a794392e4b3fd0fe07b87e78009
99474 <994749769@qq.com>
2017-12-01 15:27:05 +0800

    方便设置ViewPadding的工具类

---
+++ service/src/main/java/tf/oof/com/service/engine/ViewPaddingHelper.kt
@@ -0,0 +1,22 @@
+package tf.oof.com.service.engine
+
+import android.view.View
+
+/**
+ * Created by 99474 on 2017/12/1 0001.
+ */
+
+object ViewPaddingHelper {
+    fun setPaddingLeft(view: View, paddingLeft:Int){
+        view.setPadding(paddingLeft,view.paddingTop,view.paddingRight,view.paddingBottom)
+    }
+    fun setPaddingTop(view: View, paddingTop:Int){
+        view.setPadding(view.paddingLeft,paddingTop,view.paddingRight,view.paddingBottom)
+    }
+    fun setPaddingRight(view: View, paddingRight:Int){
+        view.setPadding(view.paddingLeft,view.paddingTop,paddingRight,view.paddingBottom)
+    }
+    fun setPaddingBottom(view: View, paddingBottom:Int){
+        view.setPadding(view.paddingLeft,view.paddingTop,view.paddingRight,paddingBottom)
+    }
+}
