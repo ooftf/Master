10d1b59da5c6c61aa34bdf492c78d04de917af0b
ooftf <994749769@qq.com>
2018-05-06 16:53:18 +0800

    动态替换图标完成

--- other/src/main/java/com/ooftf/master/other/activity/TurnIconActivity.kt
+++ other/src/main/java/com/ooftf/master/other/activity/TurnIconActivity.kt
@@ -2,6 +2,7 @@ package com.ooftf.master.other.activity
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.RadioButton
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.ooftf.master.other.R
 import com.ooftf.service.engine.AppIconEngine
@@ -13,12 +14,17 @@ class TurnIconActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_turn_icon)
-        switch_icon.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked) {
-                AppIconEngine.switchIcon(this, AppIconEngine.ICON_1)
-            } else {
-                AppIconEngine.switchIcon(this, AppIconEngine.ICON_2)
+        AppIconEngine.getAllAlias().forEach { icon ->
+            val radioButton = layoutInflater.inflate(R.layout.item_radio_item, radioGroup, false) as RadioButton
+            radioButton.text = icon
+            radioButton.setOnClickListener {
+                AppIconEngine.switchIcon(
+                        this, icon
+                )
             }
+            radioGroup.addView(radioButton)
+            radioButton.setCompoundDrawablesWithIntrinsicBounds(null, null, AppIconEngine.getDrawableFromName(this, icon), null)
+            radioButton.isChecked = icon == AppIconEngine.getCurrentAlias(this)
         }
     }
 }
--- service/src/main/java/com/ooftf/service/engine/AppIconEngine.kt
+++ service/src/main/java/com/ooftf/service/engine/AppIconEngine.kt
@@ -3,18 +3,29 @@ package com.ooftf.service.engine
 import android.content.ComponentName
 import android.content.Context
 import android.content.pm.PackageManager
+import android.graphics.drawable.Drawable
+import android.util.Log
 
-
+/**
+ * 从非alias切换到alias图标会导致APP杀死进程，解决方法：全部设置为alias
+ *
+ * 每一个<category android:name="android.intent.category.LAUNCHER" />且为true就代表一个图标
+ *
+ * 一个Application 必须有一个非alias 设置为<action android:name="android.intent.action.MAIN" /> 否则安装出错
+ */
 object AppIconEngine {
     //要跟manifest的activity-alias 的name保持一致
-    const val ICON_1 = "com.master.kit.icon_1"
-    const val ICON_2 = "com.master.kit.icon_2"
+    const val LAUNCHER_DEFAULT = "com.master.kit.icon_default"
+    const val LAUNCHER_0 = "com.master.kit.icon_0"
+    const val LAUNCHER_1 = "com.master.kit.icon_1"
+    const val LAUNCHER_2 = "com.master.kit.icon_2"
     /**
-     * @param useCode =1、为活动图标 =2 为用普通图标 =3、不启用判断
+     *
      */
-    fun switchIcon(context: Context, icon: String = ICON_1) {
-        setAliasEnable(context, ICON_1, icon == ICON_1)
-        setAliasEnable(context, ICON_2, icon == ICON_2)
+    fun switchIcon(context: Context, icon: String = LAUNCHER_0) {
+        getAllAlias().forEach {
+            setAliasEnable(context, it, icon == it)
+        }
     }
 
     /**
@@ -35,5 +46,27 @@ object AppIconEngine {
     private fun stateFromEnable(enable: Boolean): Int {
         return if (enable) PackageManager.COMPONENT_ENABLED_STATE_ENABLED
         else PackageManager.COMPONENT_ENABLED_STATE_DISABLED
+    }
+
+    fun getAllAlias(): ArrayList<String> = arrayListOf(LAUNCHER_DEFAULT, LAUNCHER_0, LAUNCHER_1, LAUNCHER_2)
+
+    fun getCurrentAlias(context: Context): String {
+        getAllAlias().forEach {
+            Log.e("Alias", "" + it)
+            Log.e("getCurrentAlias", "" + context.packageManager.getComponentEnabledSetting(ComponentName(context, it)))
+            if (context.packageManager.getComponentEnabledSetting(ComponentName(context, it)) != stateFromEnable(false)) return it
+        }
+        throw Exception("getAllAlias exception")
+    }
+
+    /**
+     * 只能获取到当前使用的图标
+     */
+    fun getDrawableFromName(context: Context, name: String): Drawable? {
+        return try {
+            context.packageManager.getActivityIcon(ComponentName(context, name))
+        } catch (e: Throwable) {
+            null
+        }
     }
 }
