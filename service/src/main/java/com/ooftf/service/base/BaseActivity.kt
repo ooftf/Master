867438c897a1bd9791c092141fc27273fdc316be
lihang9 <lihang9@jd.com>
2018-06-19 00:04:32 +0800

    更新BottomBar版本

--- service/src/main/java/com/ooftf/service/base/BaseActivity.kt
+++ service/src/main/java/com/ooftf/service/base/BaseActivity.kt
@@ -2,6 +2,8 @@ package com.ooftf.service.base
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import android.widget.Toast
