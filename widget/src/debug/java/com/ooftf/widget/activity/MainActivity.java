c055cdf876280de0d3121ad79d83a53bf1880ef9
lihang36 <lihang9@jd.com>
2018-09-03 18:32:29 +0800

    让单个moudel可以单独运行

---
+++ widget/src/debug/java/com/ooftf/widget/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.ooftf.widget.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ooftf.widget.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
