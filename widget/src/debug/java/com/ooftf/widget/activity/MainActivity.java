134ee5455cc12f8bf4244e5723100bcd5f49778a
ooftf <li994749769@gmail.com>
2018-09-03 23:10:22 +0800

    widget可以独立运行了

--- widget/src/debug/java/com/ooftf/widget/activity/MainActivity.java
+++ widget/src/debug/java/com/ooftf/widget/activity/MainActivity.java
@@ -1,15 +1,22 @@
 package com.ooftf.widget.activity;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.alibaba.android.arouter.launcher.ARouter;
+import com.ooftf.service.base.BaseActivity;
 import com.ooftf.widget.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.frameLayout, (Fragment) ARouter.getInstance().build("/widget/widget").navigation())
+                .commitAllowingStateLoss();
     }
 }
