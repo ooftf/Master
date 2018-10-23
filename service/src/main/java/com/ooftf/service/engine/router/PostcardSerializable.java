28c7cb5d4f23c5caccfb33e473f57555b89bf304
lihang36 <lihang9@jd.com>
2018-10-23 18:41:49 +0800

    添加注释

--- service/src/main/java/com/ooftf/service/engine/router/PostcardSerializable.java
+++ service/src/main/java/com/ooftf/service/engine/router/PostcardSerializable.java
@@ -6,6 +6,11 @@ import android.os.Bundle;
 import com.alibaba.android.arouter.facade.Postcard;
 import com.alibaba.android.arouter.launcher.ARouter;
 
+/**
+ * 将开启 activity的意图序列化为 Bundle 对象用于 activity之间的传递
+ * Scenes: 当某个【开启activity的意图】被 拦截器拦截掉，就会跳转到登录页面，登录成功时候要继续原来【开启activity的意图】
+ * @author lihang9
+ */
 public class PostcardSerializable {
 
     public static final String URI = "Uri";
