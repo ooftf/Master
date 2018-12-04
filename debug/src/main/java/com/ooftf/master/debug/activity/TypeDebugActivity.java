e95607c2f22e10e37349ffb208954fa912c2cd69
ooftf <li994749769@gmail.com>
2018-12-05 02:40:46 +0800

    同步代码

---
+++ debug/src/main/java/com/ooftf/master/debug/activity/TypeDebugActivity.java
@@ -0,0 +1,31 @@
+package com.ooftf.master.debug.activity;
+
+import android.os.Bundle;
+
+import com.ooftf.master.debug.R;
+import com.ooftf.service.base.BaseActivity;
+
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+
+/**
+ * @author 99474
+ * @question getGenericInterfaces的到的是什么
+ *           ParameterizedType代表的是什么
+ */
+public class TypeDebugActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_type_debug);
+
+
+    }
+
+    public static void main(String[] args) {
+        Type[] genericInterfaces = TypeDebugActivity.class.getGenericInterfaces();
+
+
+    }
+}
