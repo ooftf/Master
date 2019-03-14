c7145b757d41c270f72247874ba862548217599e
ooftf <li994749769@gmail.com>
2019-03-15 03:12:12 +0800

    sync

--- qrcode/src/main/java/com/ooftf/master/qrcode/activity/QRCodeActivity.java
+++ qrcode/src/main/java/com/ooftf/master/qrcode/activity/QRCodeActivity.java
@@ -1,21 +1,20 @@
 package com.ooftf.master.qrcode.activity;
 
-import android.os.Build;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.WindowManager;
 import android.widget.Button;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
+import com.dlazaro66.qrcodereaderview.QRCodeReaderView;
 import com.ooftf.master.qrcode.R;
 import com.ooftf.master.qrcode.utils.QrDecoder;
-import com.ooftf.master.qrcode.widget.CameraPreview;
 import com.ooftf.service.base.BaseActivity;
 import com.ooftf.service.constant.RouterPath;
 import com.ooftf.service.utils.JLog;
+import com.ooftf.service.widget.toolbar.TailoredToolbar;
 
 import org.jetbrains.annotations.Nullable;
-
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.schedulers.Schedulers;
 
 /**
  * 二维码扫描
@@ -24,10 +23,11 @@ import io.reactivex.schedulers.Schedulers;
  */
 @Route(path = RouterPath.QRCODE_ACTIVITY_QRCODE)
 public class QRCodeActivity extends BaseActivity {
-    CameraPreview cameraPreview;
+    QRCodeReaderView cameraPreview;
     Button stop;
     Button start;
     QrDecoder qrDecoder = new QrDecoder();
+    TailoredToolbar toolbar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -35,36 +35,56 @@ public class QRCodeActivity extends BaseActivity {
         setContentView(R.layout.activity_qrcode);
         start = findViewById(R.id.start);
         stop = findViewById(R.id.stop);
+        toolbar = findViewById(R.id.toolbar);
         cameraPreview = findViewById(R.id.cameraPreview);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            start.setOnClickListener(view -> {
-                cameraPreview.startPreview();
-            });
-            stop.setOnClickListener(view -> cameraPreview.stopPreview());
-        }
-        cameraPreview.setImageCallback(info -> {
-            JLog.e("length", info.getBytes().length);
-            JLog.e("getImageFormat", info.getImageFormat());//35  17
-            qrDecoder.decodePreviewByte(info.getBytes(), info.getWidth(), info.getHeight())
-                    .subscribeOn(Schedulers.computation())
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe(s -> {
-                        cameraPreview.stopPreview();
-                        JLog.e("onPreview", s);
-                    });
+        setupCameraPreview();
+        setupToolbar();
+    }
 
+    private void setupToolbar() {
+        MenuItem light = toolbar.getMenu().add("");
+        light.setIcon(R.drawable.light_close).setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+        light.setChecked(false);
+        light.setOnMenuItemClickListener(item -> {
+            item.setChecked(!item.isChecked());
+            if (item.isChecked()) {
+                item.setIcon(R.drawable.light_open);
+            } else {
+                item.setIcon(R.drawable.light_close);
+            }
+            cameraPreview.setTorchEnabled(item.isChecked());
+            return true;
         });
     }
 
+    private void setupCameraPreview() {
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        cameraPreview.setOnQRCodeReadListener((text, points) -> {
+            JLog.e(text, points);
+            cameraPreview.stopCamera();
+        });
+
+        // Use this function to enable/disable decoding
+        cameraPreview.setQRDecodingEnabled(true);
+
+        // Use this function to change the autofocus interval (default is 5 secs)
+        cameraPreview.setAutofocusInterval(2000L);
+
+        // Use this function to set back camera preview
+        cameraPreview.setBackCamera();
+    }
+
+
     @Override
-    protected void onStart() {
-        super.onStart();
-        cameraPreview.startPreview();
+    protected void onResume() {
+        cameraPreview.startCamera();
+        super.onResume();
     }
 
     @Override
-    protected void onStop() {
-        cameraPreview.stopPreview();
-        super.onStop();
+    protected void onPause() {
+        cameraPreview.stopCamera();
+        super.onPause();
     }
+
 }
