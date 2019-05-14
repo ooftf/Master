43825ec15eccd10bff4da014fcf7746749a8f539
ooftf <li994749769@gmail.com>
2019-05-14 11:04:06 +0800

    sync

--- applet/src/main/java/com/ooftf/applet/modules/duowan/Constants.java
+++ applet/src/main/java/com/ooftf/applet/modules/duowan/Constants.java
@@ -3,5 +3,6 @@ package com.ooftf.applet.modules.duowan;
 public interface Constants {
     String BASE_URL = "http://box.dwstatic.com/";
     String HOME_LIST = "http://box.dwstatic.com/apiNewsList.php?showAd=1&action=l&newsTag=headlineNews&p=1";
+    String BASE_MAIN = "http://lol.duowan.com/";
 
 }
--- applet/src/main/java/com/ooftf/applet/modules/duowan/DuowanMainViewHolder.java
+++ applet/src/main/java/com/ooftf/applet/modules/duowan/DuowanMainViewHolder.java
@@ -12,9 +12,14 @@ import com.ooftf.service.constant.RouterPath;
 import me.tatarka.bindingcollectionadapter2.ItemBinding;
 
 public class DuowanMainViewHolder {
-    public final OnItemClickListener<MainListResponse.DataBean> listener = data -> ARouter.getInstance().build(RouterPath.Web.Activity.MAIN).withString("url",data.getDestUrl()).navigation();
+    public final OnItemClickListener<MainListResponse.DataBean> listener = data -> ARouter.getInstance().build(RouterPath.Web.Activity.MAIN).withString("url", genWebUrl(data)).navigation();
     public final ObservableList<MainListResponse.DataBean> items = new ObservableArrayList<>();
-    public final ItemBinding<MainListResponse.DataBean> itemBinding = ItemBinding.<MainListResponse.DataBean>of(BR.item, R.layout.item_duowan_main).bindExtra(BR.listener,listener);
+    public final ItemBinding<MainListResponse.DataBean> itemBinding = ItemBinding.<MainListResponse.DataBean>of(BR.item, R.layout.item_duowan_main).bindExtra(BR.listener, listener);
 
 
+    String genWebUrl(MainListResponse.DataBean bean) {
+        String url = bean.getCommentUrl();
+        int position = url.indexOf("&");
+        return Constants.BASE_MAIN + url.substring(0, position) + ".html";
+    }
 }
