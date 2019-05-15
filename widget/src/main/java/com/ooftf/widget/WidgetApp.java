17c3fcd7bd56f82d8a90e7c13f4c36f146f52fc4
ooftf <994749769@qq.com>
2019-05-16 02:09:36 +0800

    sync

--- widget/src/main/java/com/ooftf/widget/WidgetApp.java
+++ widget/src/main/java/com/ooftf/widget/WidgetApp.java
@@ -20,6 +20,7 @@ import com.scwang.smartrefresh.layout.api.RefreshHeader;
 import com.scwang.smartrefresh.layout.api.RefreshLayout;
 import com.scwang.smartrefresh.layout.footer.ClassicsFooter;
 import com.scwang.smartrefresh.layout.header.ClassicsHeader;
+import com.scwang.smartrefresh.layout.header.TwoLevelHeader;
 
 import java.util.ArrayList;
 
@@ -33,7 +34,7 @@ public class WidgetApp implements IApplication {
     static {
         //设置全局的Header构建器
         SmartRefreshLayout.setDefaultRefreshHeaderCreator((context, layout) -> {
-            layout.setPrimaryColorsId(R.color.colorPrimary, android.R.color.white);//全局设置主题颜色
+           // layout.setPrimaryColorsId(android.R.color.white, R.color.font_black);//全局设置主题颜色
             return new ClassicsHeader(context);//.setTimeFormat(new DynamicTimeFormat("更新于 %s"));//指定为经典Header，默认是 贝塞尔雷达Header
         });
         //设置全局的Footer构建器
