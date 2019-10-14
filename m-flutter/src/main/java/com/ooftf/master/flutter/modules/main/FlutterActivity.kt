cdaddb2b1aba043cee851e35c4c795229036ce1a
ooftf <li994749769@gmail.com>
2019-10-14 17:54:38 +0800

    规范命名

---
+++ m-flutter/src/main/java/com/ooftf/master/flutter/modules/main/FlutterActivity.kt
@@ -0,0 +1,31 @@
+package com.ooftf.master.flutter.modules.main
+
+import android.os.Bundle
+import android.widget.FrameLayout
+import com.alibaba.android.arouter.facade.annotation.Route
+import com.ooftf.service.base.BaseActivity
+import io.flutter.facade.Flutter
+
+/**
+ *
+ * @author ooftf
+ * @email 994749769@qq.com
+ * @date 2019/10/14
+ */
+@Route(path = "/flutter/main")
+class FlutterActivity :BaseActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        /*    setContentView(R.layout.activity_share)
+    wbShare.setOnClickListener {
+        share()
+    }*/
+        val flutterView = Flutter.createView(
+                this,
+                lifecycle,
+                "route1"
+        )
+        val layout = FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT)
+        addContentView(flutterView, layout)
+    }
+}
