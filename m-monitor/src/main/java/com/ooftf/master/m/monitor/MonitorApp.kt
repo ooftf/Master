77b2db9345ed61b529499af60c6cfda89d6247d7
ooftf <li994749769@gmail.com>
2019-10-25 19:48:54 +0800

    修复bug

--- m-monitor/src/main/java/com/ooftf/master/m/monitor/MonitorApp.kt
+++ m-monitor/src/main/java/com/ooftf/master/m/monitor/MonitorApp.kt
@@ -11,6 +11,8 @@ import com.ooftf.service.base.BaseApplication
 import com.ooftf.service.utils.ProcessUtils
 import com.ooftf.service.utils.ThreadUtil
 import com.tencent.bugly.crashreport.CrashReport
+import com.tendcloud.tenddata.TCAgent
+
 //import com.zhuge.analysis.stat.ZhugeSDK
 
 /**
@@ -32,7 +34,7 @@ class MonitorApp : IApplication {
             }
 
             override fun onActivityDestroyed(activity: Activity) {
-                if(activity.javaClass.name == "com.ooftf.master.m.entrance.ui.MainActivity"){
+                if (activity.javaClass.name == "com.ooftf.master.m.entrance.ui.MainActivity") {
                     //ZhugeSDK.getInstance().flush(activity?.applicationContext)
                 }
 
@@ -45,13 +47,20 @@ class MonitorApp : IApplication {
             }
 
             override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
-                if(activity.javaClass.name == "com.ooftf.master.m.entrance.ui.MainActivity"){
+                if (activity.javaClass.name == "com.ooftf.master.m.entrance.ui.MainActivity") {
                     //ZhugeSDK.getInstance().init(activity?.applicationContext)
                 }
 
             }
 
         })
+
+        TCAgent.LOG_ON = true
+        // App ID: 在TalkingData创建应用后，进入数据报表页中，在“系统设置”-“编辑应用”页面里查看App ID。
+        // 渠道 ID: 是渠道标识符，可通过不同渠道单独追踪数据。
+        TCAgent.init(BaseApplication.instance, "FDA79E8C0AED42E09B1C89BB6C86C877", "Undefined")
+        // 如果已经在AndroidManifest.xml配置了App ID和渠道ID，调用TCAgent.init(this)即可；或与AndroidManifest.xml中的对应参数保持一致。
+        TCAgent.setReportUncaughtExceptions(false)
         //        /BaseApplication.instance
     }
 
