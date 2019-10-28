2f2eb158f4e141b41421a40fc2bfd6a6ea826844
ooftf <li994749769@gmail.com>
2019-10-28 18:28:55 +0800

    添加talkingdata事件监控

--- m-monitor/src/main/java/com/ooftf/master/m/monitor/MonitorApp.kt
+++ m-monitor/src/main/java/com/ooftf/master/m/monitor/MonitorApp.kt
@@ -55,17 +55,18 @@ class MonitorApp : IApplication {
 
         })
 
-        TCAgent.LOG_ON = true
-        // App ID: 在TalkingData创建应用后，进入数据报表页中，在“系统设置”-“编辑应用”页面里查看App ID。
-        // 渠道 ID: 是渠道标识符，可通过不同渠道单独追踪数据。
-        TCAgent.init(BaseApplication.instance, "FDA79E8C0AED42E09B1C89BB6C86C877", "Undefined")
-        // 如果已经在AndroidManifest.xml配置了App ID和渠道ID，调用TCAgent.init(this)即可；或与AndroidManifest.xml中的对应参数保持一致。
-        TCAgent.setReportUncaughtExceptions(false)
-        //        /BaseApplication.instance
+
     }
 
     override fun onCreate() {
         initBugly(BaseApplication.instance)
+        TCAgent.LOG_ON = true
+        // App ID: 在TalkingData创建应用后，进入数据报表页中，在“系统设置”-“编辑应用”页面里查看App ID。
+        // 渠道 ID: 是渠道标识符，可通过不同渠道单独追踪数据。
+        TCAgent.init(BaseApplication.instance)
+        // 如果已经在AndroidManifest.xml配置了App ID和渠道ID，调用TCAgent.init(this)即可；或与AndroidManifest.xml中的对应参数保持一致。
+        TCAgent.setReportUncaughtExceptions(false)
+        //        /BaseApplication.instance
     }
 
     override fun onLowMemory() {
