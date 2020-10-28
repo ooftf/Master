008adfe4f0c642d9cef80532e28148cae52f8547
ooftf <li994749769@gmail.com>
2020-10-28 15:51:31 +0800

    升级smart

--- m-other/src/main/java/com/ooftf/master/other/activity/ShareActivity.kt
+++ m-other/src/main/java/com/ooftf/master/other/activity/ShareActivity.kt
@@ -3,9 +3,9 @@ package com.ooftf.master.other.activity
 import android.graphics.BitmapFactory
 import android.os.Bundle
 import com.alibaba.android.arouter.facade.annotation.Route
+import com.ooftf.arch.frame.mvvm.activity.BaseActivity
 import com.ooftf.hishare.HiShare
 import com.ooftf.master.other.R
-import com.ooftf.arch.frame.mvvm.activity.BaseActivity
 import com.ooftf.service.widget.dialog.ShareDialog
 import kotlinx.android.synthetic.main.activity_share.*
 
@@ -22,13 +22,13 @@ class ShareActivity : BaseActivity() {
     }
 
     private fun share() {
-        var dialog = ShareDialog(this);
-        var shareData = ShareDialog.ShareData()
+        val dialog = ShareDialog(this);
+        val shareData = ShareDialog.ShareData()
         shareData.params = HiShare.ShareParams("http://www.baidu.com", "标题", "这是个内容", null, BitmapFactory.decodeResource(resources, com.ooftf.master.other.R.drawable.logo_legacy))
         shareData.channelShare.add(ShareDialog.ShareItem("QQ好友", R.mipmap.ic_qq, HiShare.ShareType.QQ_FRIEND))
         shareData.channelShare.add(ShareDialog.ShareItem("微信好友", R.mipmap.ic_wx_freind, HiShare.ShareType.WX_FRIEND))
         shareData.channelShare.add(ShareDialog.ShareItem("微信朋友圈", R.mipmap.ic_wx_moment, HiShare.ShareType.WX_MOMENT))
-        dialog.setData(shareData);
+        dialog.setData(shareData)
         dialog.show()
     }
 }
