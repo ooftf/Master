27860e633a48aac201cb9815ffc48117c3434658
ooftf <994749769@qq.com>
2021-12-01 00:17:30 +0800

    Could not find com.tencent.imsdk:imsdk-smart:0.0.1006.

--- buildSrc/src/main/java/Dokit.kt
+++
@@ -1,3 +0,0 @@
-object Dokit {
-    const val plugin = "io.github.didi.dokit:dokitx-plugin:3.5.0"
-}
--- buildSrc/src/main/java/Plugins.kt
+++ buildSrc/src/main/java/Plugins.kt
@@ -1,3 +1,4 @@
 object Plugins {
     const val autoregister = "com.github.ooftf:autoregister:1.4.4"
+    const val dokit = "io.github.didi.dokit:dokitx-plugin:3.5.0"
 }
